d54f4a2c4a653c2fc8bb1189dfcaa10f3e7852e6
Joel Banzatto <joel.banzatto@db1.com.br>
2021-08-03 12:28:01 -0300

    primeiro commit

---
+++ IntegrandoBitrise/IntegrandoBitriseApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntegrandoBitriseApp.swift
+//  IntegrandoBitrise
+//
+//  Created by Joel Banzatto on 03/08/21.
+//
+
+import SwiftUI
+
+@main
+struct IntegrandoBitriseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
